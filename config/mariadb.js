aef94c81d1cb61dcc382e10b864f6aa1b110dbc3
zackieAnalyzt <analyztman@gmail.com>
2025-06-30 14:59:56 +0700

    Update mariadb.js
    เปลี่ยน config mariadb

--- config/mariadb.js
+++ config/mariadb.js
@@ -2,9 +2,9 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: '192.168.100.10',
-  user: 'lee',
-  password: '11142',
+  host: '192.168.100.11',
+  user: 'leeuser',
+  password: 'hosxp11142',
   database: 'lee',
   connectionLimit: 5,
   charset: 'tis620'
